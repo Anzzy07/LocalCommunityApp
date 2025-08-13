6c9a9afcf8e87d8e99b161eeae906e7e284e089c
Anzzy07 <anzel.acharya307@gmail.com>
2025-08-13 19:46:35 +0545

    Setup

---
+++ src/app/about.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+export default function about() {
+  return (
+    <View>
+      <Link href="index">index</Link>
+    </View>
+  );
+}
